ba04ae3f306c06d51dcd729213345969e0a2defd
Bexultan Alipov <82113002+jacksultan-Airport@users.noreply.github.com>
2022-05-15 15:28:46 +0200

    Add files via upload

---
+++ Pathfinding/Scripts/PathfindingDebugStepVisual.cs
@@ -0,0 +1,218 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+
+
+// I have no idea how it works
+
+
+//After reading the code for like 20 minute I'm not able to explain how it actually works
+// But I cam tell that it takes all the values of grid and pathalgorithm and shows visual animation... 
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using CodeMonkey.Utils;
+
+public class PathfindingDebugStepVisual : MonoBehaviour {
+
+    public static PathfindingDebugStepVisual Instance { get; private set; }
+
+    [SerializeField] private Transform pfPathfindingDebugStepVisualNode;
+    private List<Transform> visualNodeList;
+    private List<GridSnapshotAction> gridSnapshotActionList;
+    private bool autoShowSnapshots;
+    private float autoShowSnapshotsTimer;
+    private Transform[,] visualNodeArray; 
+
+    private void Awake() {
+        Instance = this;
+        visualNodeList = new List<Transform>();
+        gridSnapshotActionList = new List<GridSnapshotAction>();
+    }
+
+    public void Setup(Grid<PathNode> grid) {
+        visualNodeArray = new Transform[grid.GetWidth(), grid.GetHeight()];
+
+        for (int x = 0; x < grid.GetWidth(); x++) {
+            for (int y = 0; y < grid.GetHeight(); y++) {
+                Vector3 gridPosition = new Vector3(x, y) * grid.GetCellSize() + Vector3.one * grid.GetCellSize() * .5f;
+                Transform visualNode = CreateVisualNode(gridPosition);
+                visualNodeArray[x, y] = visualNode;
+                visualNodeList.Add(visualNode);
+            }
+        }
+        HideNodeVisuals();
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            ShowNextSnapshot();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            autoShowSnapshots = true;
+        }
+
+        if (autoShowSnapshots) {
+            float autoShowSnapshotsTimerMax = 0.0005f; //after poking around the code I found the timer of snapshots so i can make it faster or slower
+            autoShowSnapshotsTimer -= Time.deltaTime;
+            if (autoShowSnapshotsTimer <= 0f) {
+                autoShowSnapshotsTimer += autoShowSnapshotsTimerMax;
+                ShowNextSnapshot();
+                if (gridSnapshotActionList.Count == 0) {
+                    autoShowSnapshots = false;
+                }
+            }
+        }
+    }
+
+    private void ShowNextSnapshot() {
+        if (gridSnapshotActionList.Count > 0) {
+            GridSnapshotAction gridSnapshotAction = gridSnapshotActionList[0];
+            gridSnapshotActionList.RemoveAt(0);
+            gridSnapshotAction.TriggerAction();
+        }
+    }
+
+    public void ClearSnapshots() {
+        gridSnapshotActionList.Clear();
+    }
+    //Takes grid, pathnode pathfinding objects to make animation then stuff...
+    public void TakeSnapshot(Grid<PathNode> grid, PathNode current, List<PathNode> openList, List<PathNode> closedList) {
+        GridSnapshotAction gridSnapshotAction = new GridSnapshotAction();
+        gridSnapshotAction.AddAction(HideNodeVisuals);
+        
+        for (int x = 0; x < grid.GetWidth(); x++) {
+            for (int y = 0; y < grid.GetHeight(); y++) {
+                PathNode pathNode = grid.GetGridObject(x, y);
+
+                int gCost = pathNode.gCost;
+                int hCost = pathNode.hCost;
+                int fCost = pathNode.fCost;
+                Vector3 gridPosition = new Vector3(pathNode.x, pathNode.y) * grid.GetCellSize() + Vector3.one * grid.GetCellSize() * .5f;
+                bool isCurrent = pathNode == current;
+                bool isInOpenList = openList.Contains(pathNode);
+                bool isInClosedList = closedList.Contains(pathNode);
+                int tmpX = x;
+                int tmpY = y;
+
+                gridSnapshotAction.AddAction(() => {
+                    Transform visualNode = visualNodeArray[tmpX, tmpY];
+                    SetupVisualNode(visualNode, gCost, hCost, fCost);
+
+                    Color backgroundColor = UtilsClass.GetColorFromString("636363");
+
+                    if (isInClosedList) {
+                        backgroundColor = new Color(1, 0, 0);
+                    }
+                    if (isInOpenList) {
+                        backgroundColor = UtilsClass.GetColorFromString("009AFF");
+                    }
+                    if (isCurrent) {
+                        backgroundColor = new Color(0, 1, 0);
+                    }
+
+                    visualNode.Find("sprite").GetComponent<SpriteRenderer>().color = backgroundColor;
+                });
+            }
+        }
+
+        gridSnapshotActionList.Add(gridSnapshotAction);
+    }
+
+    public void TakeSnapshotFinalPath(Grid<PathNode> grid, List<PathNode> path) {
+        GridSnapshotAction gridSnapshotAction = new GridSnapshotAction();
+        gridSnapshotAction.AddAction(HideNodeVisuals);
+        
+        for (int x = 0; x < grid.GetWidth(); x++) {
+            for (int y = 0; y < grid.GetHeight(); y++) {
+                PathNode pathNode = grid.GetGridObject(x, y);
+
+                int gCost = pathNode.gCost;
+                int hCost = pathNode.hCost;
+                int fCost = pathNode.fCost;
+                Vector3 gridPosition = new Vector3(pathNode.x, pathNode.y) * grid.GetCellSize() + Vector3.one * grid.GetCellSize() * .5f;
+                bool isInPath = path.Contains(pathNode);
+                int tmpX = x;
+                int tmpY = y;
+
+                gridSnapshotAction.AddAction(() => { 
+                    Transform visualNode = visualNodeArray[tmpX, tmpY];
+                    SetupVisualNode(visualNode, gCost, hCost, fCost);
+
+                    Color backgroundColor;
+
+                    if (isInPath) {
+                        backgroundColor = new Color(0, 1, 0);
+                    } else {
+                        backgroundColor = UtilsClass.GetColorFromString("636363");
+                    }
+
+                    visualNode.Find("sprite").GetComponent<SpriteRenderer>().color = backgroundColor;
+                });
+            }
+        }
+
+        gridSnapshotActionList.Add(gridSnapshotAction);
+    }
+
+    private void HideNodeVisuals() {
+        foreach (Transform visualNodeTransform in visualNodeList) {
+            SetupVisualNode(visualNodeTransform, 9999, 9999, 9999);
+        }
+    }
+
+    private Transform CreateVisualNode(Vector3 position) {
+        Transform visualNodeTransform = Instantiate(pfPathfindingDebugStepVisualNode, position, Quaternion.identity);
+        return visualNodeTransform;
+    }
+
+    private void SetupVisualNode(Transform visualNodeTransform, int gCost, int hCost, int fCost)
+    {
+        if (fCost < 1000)
+        {
+
+            // What I understood here is that prints to cells g,f,h costs
+            visualNodeTransform.Find("gCostText").GetComponent<TextMeshPro>().SetText(gCost.ToString());
+            visualNodeTransform.Find("hCostText").GetComponent<TextMeshPro>().SetText(hCost.ToString());
+            visualNodeTransform.Find("fCostText").GetComponent<TextMeshPro>().SetText(fCost.ToString());
+        }
+        else
+        {
+            visualNodeTransform.Find("gCostText").GetComponent<TextMeshPro>().SetText("");
+            visualNodeTransform.Find("hCostText").GetComponent<TextMeshPro>().SetText("");
+            visualNodeTransform.Find("fCostText").GetComponent<TextMeshPro>().SetText("");
+        }
+    }
+
+    private class GridSnapshotAction {
+
+        private Action action;
+
+        public GridSnapshotAction() {
+            action = () => { };
+        }
+
+        public void AddAction(Action action) {
+            this.action += action;
+        }
+
+        public void TriggerAction() {
+            action();
+        }
+
+    }
+
+}
+
---
+++ Pathfinding/Scripts/Testing.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+using CodeMonkey;
+
+public class Testing : MonoBehaviour {
+    
+    [SerializeField] private PathfindingDebugStepVisual pathfindingDebugStepVisual;
+    [SerializeField] private PathfindingVisual pathfindingVisual;
+   
+    private Pathfinding pathfinding;
+
+    private void Start() {
+        //                            height and width of the cells
+        pathfinding = new Pathfinding(30, 30);
+        pathfindingDebugStepVisual.Setup(pathfinding.GetGrid());
+        pathfindingVisual.SetGrid(pathfinding.GetGrid());
+    }
+
+    private void Update() {
+        //left mouse click event
+        if (Input.GetMouseButtonDown(0)) {
+
+            // Gets mouse position from the camera perspective and mouse x,y position, was coded in utiliti
+            Vector3 mouseWorldPosition = UtilsClass.GetMouseWorldPosition();
+            //Passes that value to the pathfinding algorithm 
+            pathfinding.GetGrid().GetXY(mouseWorldPosition, out int x, out int y);
+            // then we call calculated list of best nodes with least f cost. 
+            // Find Path(0,0,x,y) 0, 0 starting point, x,y value from mouse world position
+            List<PathNode> path = pathfinding.FindPath(5, 5, x, y);
+            //Draws line if click
+            //Goes through the list of nodes then draws lines
+            if (path != null) {
+                for (int i=0; i<path.Count - 1; i++) {
+                    Debug.DrawLine(new Vector3(path[i].x, path[i].y) * 10f + Vector3.one * 5f, new Vector3(path[i+1].x, path[i+1].y) * 10f + Vector3.one * 5f, Color.green, 5f);
+                }
+            }
+            
+        }
+
+        //Right click event
+        //Sets unwalkable nodes
+        if (Input.GetMouseButtonDown(1)) {
+            // Gets mouse position from the camera perspective and mouse x,y position, was coded in utiliti
+            Vector3 mouseWorldPosition = UtilsClass.GetMouseWorldPosition();
+            pathfinding.GetGrid().GetXY(mouseWorldPosition, out int x, out int y);
+            //Sets unwalkable nodes
+            pathfinding.GetNode(x, y).SetIsWalkable(!pathfinding.GetNode(x, y).isWalkable);
+        }
+    }
+
+}
